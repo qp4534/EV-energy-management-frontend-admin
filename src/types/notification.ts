d72111ea35b71950327de4305311389aab663b5c
codmss <oho8404@gmail.com>
2026-07-29 17:46:03 +0900

    fix : 알림 수정

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,7 +1,11 @@
+export type NotiType = '긴급' | '경고' | '주의' | '정상';
+
 export type Notification = {
   id: string;
   title: string;
   body: string;
   isRead: boolean;
   createdAt: string;
+  type?: NotiType; 
+  hasReport?: boolean;
 };
