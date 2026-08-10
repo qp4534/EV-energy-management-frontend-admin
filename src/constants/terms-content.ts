ed701bc9f762b653331353239e5f983738597cf7
minjomiya <mjk030505@gmail.com>
2026-08-10 10:52:23 +0900

    담당자 메일 수정

--- src/constants/terms-content.ts
+++ src/constants/terms-content.ts
@@ -449,7 +449,7 @@ export const TERMS: TermContent[] = [
             columns: ['구분', '내용'],
             rows: [
               ['개인정보보호책임자', 'MijungE 개인정보보호팀'],
-              ['연락처', 'privacy@MijungE.com'],
+              ['연락처', 'tkyaho@mijungev.kro.kr'],
             ],
           },
           {
@@ -552,7 +552,7 @@ export const TERMS: TermContent[] = [
             columns: ['구분', '내용'],
             rows: [
               ['성명', '고길동'],
-              ['연락처', 'privacy@MijungE.com'],
+              ['연락처', 'tkyaho@mijungev.kro.kr'],
             ],
           },
         ],
